31a644b2ba967210efa912db466cb09ea016d643
DjinnSystemsMaster <bsullivan0069@gmail.com>
2025-04-07 16:40:48 -0400

    Create recursive_agent.py

---
+++ model/recursive_agent.py
@@ -0,0 +1,19 @@
+class RecursiveAgent:
+    def __init__(self, freeze_threshold):
+        self.state = None
+        self.freeze_threshold = freeze_threshold
+        self.locked = False
+        self.history = []
+
+    def update(self, input_signal, entropy):
+        if entropy > self.freeze_threshold:
+            self.locked = True
+            return self.state  # Freeze-Lock activated
+        if self.locked and entropy <= self.freeze_threshold:
+            self.locked = False  # Unlock on stability
+        self.state = self.recursive_transition(input_signal)
+        self.history.append(self.state)
+        return self.state
+
+    def recursive_transition(self, input_signal):
+        return input_signal * 0.9 if self.state is None else self.state * 0.9 + input_signal * 0.1
