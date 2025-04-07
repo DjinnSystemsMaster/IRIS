bd7836c37d67edcae0b9e7d1c1ee9a71821ab3a1
DjinnSystemsMaster <bsullivan0069@gmail.com>
2025-04-07 16:42:42 -0400

    Create coherence.py

---
+++ metrics/coherence.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def compute_coherence(state_history):
+    if len(state_history) < 2:
+        return 1.0
+    v1 = np.array(state_history[-2])
+    v2 = np.array(state_history[-1])
+    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2) + 1e-9)
